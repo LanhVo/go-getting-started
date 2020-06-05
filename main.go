d1d98354cf77faf576bddb2944eba911549ead96
Lanh Vo <lanhvo@nvg.vn>
2020-06-05 10:54:02 +0700

    Add Verify Receipt for Appstore
    Add Verify Receipt for Appstore

--- main.go
+++ main.go
@@ -1,29 +1,58 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/awa/go-iap/appstore"
+	_ "github.com/heroku/x/hmetrics/onload"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	http.HandleFunc("/appstore", verifyAppstoreReceipt)
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	fmt.Printf("Starting server for verify receipt...\n")
+	if err := http.ListenAndServe(":5000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type ReceiptData struct {
+	Receipt string
+}
+
+
+func verifyAppstoreReceipt(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/appstore" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	switch r.Method {
+	case "POST":
+		var receipt ReceiptData
+		err := json.NewDecoder(r.Body).Decode(&receipt)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+		client := appstore.New()
+		req := appstore.IAPRequest{
+			ReceiptData: receipt.Receipt,
+		}
+		resp := &appstore.IAPResponse{}
+		ctx := context.Background()
+		error := client.Verify(ctx, req, resp)
+		if error != nil {
+			fmt.Fprintf(w, "Failed = %v\n", error)
+		} else {
+			fmt.Fprintf(w, "Success = %v\n", resp)
+		}
 
-	router.Run(":" + port)
+	default:
+		fmt.Fprintf(w, "Sorry, only POST methods are supported.")
+	}
 }
